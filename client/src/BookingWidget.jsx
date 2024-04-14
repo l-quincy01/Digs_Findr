bdb649736d62afdb4efa788f1b22d60ba125cc3a
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-04-14 12:23:38 +0200

    More stylistic changes

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -3,6 +3,12 @@ import { differenceInCalendarDays } from "date-fns";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import { LuCalendarDays } from "react-icons/lu";
+import { IoMdInformationCircleOutline, IoMdStopwatch } from "react-icons/io";
+import { BiMessageDetail } from "react-icons/bi";
+import { LuBadgeCheck } from "react-icons/lu";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { FaRegFlag, FaRegHeart } from "react-icons/fa";
 
 export default function BookingWidget({ place }) {
   const [checkIn, setCheckIn] = useState();
@@ -11,6 +17,9 @@ export default function BookingWidget({ place }) {
   const [name, setName] = useState();
   const [phoneNumber, setPhoneNumber] = useState();
   const [redirect, setRedirect] = useState();
+  const [showAgent, setShowAgent] = useState(false);
+  const [agentName, setAgentName] = useState();
+  const [agentPhoneNumber, setAgentPhoneNumber] = useState();
   const { user } = useContext(UserContext);
 
   useEffect(() => {
@@ -19,7 +28,15 @@ export default function BookingWidget({ place }) {
     }
   }, [user]);
 
+  const handleShowAgents = () => {
+    setShowAgent(true);
+  };
+  const handleHideAgents = () => {
+    setShowAgent(false);
+  };
+
   let numberOfDays = 0;
+
   if (checkIn && checkOut) {
     numberOfDays = differenceInCalendarDays(
       new Date(checkOut),
@@ -46,12 +63,17 @@ export default function BookingWidget({ place }) {
   }
 
   return (
-    <div className="my-2">
-      <div className="bg-white shadow p-4 rounded-2xl">
-        <div className="text-2xl text-center">
-          Price: R{place.price} / night
-        </div>
-        <div className=" border rounded-2xl mt-4">
+    <div className="my-2 sticky">
+      <div className="bg-white shadow rounded-2xl">
+        <div className="p-4">
+          <div className="text-xl  text-left font-bold ">
+            Price: R {place.price}{" "}
+            <span className="text-sm font-semibold text-gray-500">
+              {" "}
+              / month
+            </span>
+          </div>
+          {/* <div className=" border rounded-2xl mt-4">
           <div className="flex items-center justify-evenly">
             <div className=" px-4 py-3 ">
               <label> Check in: </label>
@@ -98,9 +120,9 @@ export default function BookingWidget({ place }) {
               />
             </div>
           )}
-        </div>
+        </div> */}
 
-        <div className="mt-2">
+          {/* <div className="mt-2">
           {numberOfDays > 0 ? (
             <div className="text-gray-500 flex">
               <span className=" underline ">
@@ -114,11 +136,105 @@ export default function BookingWidget({ place }) {
               Number of days must be greater than 0.
             </div>
           )}
-        </div>
+        </div> */}
 
-        <button onClick={bookPlace} className="primary mt-4">
-          Book This Place
-        </button>
+          <div className="  rounded-2xl mt-6">
+            <div className=" py-4 px-4  items-center justify-evenly  border-t  ">
+              Your Name:
+              <input
+                className=""
+                type="text"
+                value={name}
+                onChange={(ev) => setName(ev.target.value)}
+              />
+              Your Phone Number:
+              <input
+                className=""
+                type="text"
+                value={phoneNumber}
+                onChange={(ev) => setPhoneNumber(ev.target.value)}
+              />
+              Your Email:
+              <input
+                className=""
+                type="text"
+                value={phoneNumber}
+                //   onChange={(ev) => setPhoneNumber(ev.target.value)}
+              />
+              Message:
+              <input
+                className=""
+                type="text"
+                value={phoneNumber}
+                //   onChange={(ev) => setPhoneNumber(ev.target.value)}
+              />
+            </div>
+          </div>
+          <div className="grid gap-x-4 grid-cols-[2fr_1fr] ]">
+            <button onClick={bookPlace} className="primary my-2 flex-grow">
+              Send Enquiry
+            </button>
+            <button
+              onClick={bookPlace}
+              className=" bg-white border flex gap-2 items-center justify-center flex-grow h-full"
+            >
+              Save <FaRegHeart />
+            </button>
+
+            <div>
+              {!showAgent && (
+                <button onClick={handleShowAgents} className="primary w-2/3 ">
+                  Show Agent Contact numbers
+                </button>
+              )}
+
+              {showAgent && (
+                <>
+                  <button onClick={handleHideAgents} className="primary w-2/3 ">
+                    Hide Agent Contact numbers
+                  </button>
+                  <div className="py-4 px-4 items-center justify-evenly border-t">
+                    Name: AGENT NAME <br />
+                    Number: AGENT NUMBER
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className=" border-t mt-2 py-2">
+            <span className=" font-bold">Landlord Name</span>
+            <div className="flex">
+              <img
+                className=" w-1/4  rounded-full  "
+                src="https://digsconnect.imgix.net/profile_pictures/image_cropper_1599924511152.jpg?fit=crop&h=200&ixlib=python-4.0.0&w=200"
+                alt="Landlord image"
+              />
+              <div className=" flex flex-col justify-center ml-4">
+                <label className="flex gap-2 items-baseline">
+                  <LuCalendarDays /> Joined Digs Findr{" "}
+                  <span className="font-bold"> Feb 2024</span>
+                </label>
+                <label className="flex gap-2 items-baseline">
+                  <IoMdStopwatch /> Response Time
+                  <IoMdInformationCircleOutline className="scale-125 right-12 absolute cursor-pointer" />
+                </label>
+                <label className="flex gap-2 items-baseline">
+                  <BiMessageDetail /> Reply Rate{" "}
+                  <IoMdInformationCircleOutline className="scale-125 right-12 absolute cursor-pointer" />
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-2 w-full h-1/10 bg-cyan-600 text-white rounded-b-2xl font-bold p-4">
+          <LuBadgeCheck />
+          Trusted Landlord
+          <IoMdInformationCircleOutline className="scale-125 right-12 absolute cursor-pointer" />
+        </div>
+      </div>
+      <div className="flex underline gap-3 items-center justify-center my-5 cursor-pointer">
+        <FaRegFlag /> Report Listing
       </div>
     </div>
   );
--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -4,6 +4,18 @@ import { useParams } from "react-router-dom";
 import BookingWidget from "../BookingWidget";
 import PlaceGallery from "../PlaceGallery";
 import LocationAddress from "../LocationAddress";
+import { HiBuildingLibrary, HiCheckBadge, HiHome } from "react-icons/hi2";
+import { PiCertificate, PiCouch } from "react-icons/pi";
+import { IoWifi } from "react-icons/io5";
+import { FaStar, FaSwimmingPool } from "react-icons/fa";
+import { MdOutlineBedroomChild } from "react-icons/md";
+import { HiHomeModern } from "react-icons/hi2";
+import { MdApartment } from "react-icons/md";
+import { CgGym } from "react-icons/cg";
+import { MdDeck } from "react-icons/md";
+import { GoPeople } from "react-icons/go";
+import { PiBedBold } from "react-icons/pi";
+import { MdOutlineYard } from "react-icons/md";
 
 export default function PlacePage() {
   const { id } = useParams();
@@ -29,6 +41,16 @@ export default function PlacePage() {
       <div className=" mt-8 gap-8 grid grid-cols-1  md:grid-cols-[2fr_1fr]">
         <div>
           <h1 className="text-2xl">{place.title}</h1>
+          <div className="text-primary my-2"> Property Type</div>
+          <div className="flex gap-2 items-center font-semibold">
+            <FaStar /> 4.42 ·{" "}
+            <span className="underline cursor-pointer"> 61 Reviews </span>
+          </div>
+          <LocationAddress>{place.address}</LocationAddress>
+          <div className="font-semibold">
+            1 Km Away from{" "}
+            <span className="underline cursor-pointer">Monash</span>
+          </div>
           <div className="my-4">
             <h2 className="fonr-semibold text-2xl">Description</h2>
             {place.description}
@@ -36,12 +58,58 @@ export default function PlacePage() {
           {/* Check-in: {place.checkIn} <br />
           Check-out: {place.checkOut} <br />
           Max number of guests: {place.maxGuests} */}
+          <h3 className="fonr-semibold text-2xl">What this place offers</h3>
+          <div className="grid  grid-cols-3 gap-2 md:grid-cols-4 py-4">
+            <label className="flex items-baseline gap-2">
+              <HiHome /> House
+            </label>
+            <label className="flex items-baseline gap-2">
+              <HiHomeModern /> Modern House
+            </label>
+            <label className="flex items-baseline gap-2">
+              <MdApartment /> Apartment
+            </label>
+            <label className="flex items-baseline gap-2">
+              <IoWifi /> Wi Fi included
+            </label>
+            <label className="flex items-baseline gap-2">
+              <PiCertificate /> NSFAS Accredited
+            </label>
+            <label className="flex items-baseline gap-2">
+              <MdOutlineBedroomChild /> Bachelor
+            </label>
+            <label className="flex items-baseline gap-2">
+              <PiBedBold /> 1* Bedroom
+            </label>
+            <label className="flex items-baseline gap-2">
+              <GoPeople /> Shared
+            </label>
+            <label className="flex items-baseline gap-2">
+              <PiCouch /> Furnished
+            </label>
+            <label className="flex items-baseline gap-2">
+              <MdOutlineYard /> Yard
+            </label>
+            <label className="flex items-baseline gap-2">
+              <FaSwimmingPool /> Swimming Pool
+            </label>
+            <label className="flex items-baseline gap-2">
+              <CgGym /> Gym
+            </label>
+            <label className="flex items-baseline gap-2">
+              <MdDeck /> Deck
+            </label>
+          </div>
+          <h2 className="fonr-semibold text-2xl">Things to know</h2>
+          {place.description}
         </div>
-        <BookingWidget place={place} />
-        <div className="mt-2 text-sm gray-800 leading-4">
+        <div className="  sticky">
+          <BookingWidget place={place} />
+        </div>
+        {/* <div className="mt-2 text-sm gray-800 leading-4">
           <h2 className="fonr-semibold text-2xl">Things to know</h2>
           {place.extraInfo}
-        </div>
+        </div> */}
       </div>
     </div>
   );
