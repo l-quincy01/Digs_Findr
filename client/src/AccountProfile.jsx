e9339db4c3842c4d6ac0348d8fda8deaa6b28a95
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-04-18 08:22:37 +0200

    More stylistic changes

---
+++ client/src/AccountProfile.jsx
@@ -0,0 +1,12 @@
+import { IoStarSharp } from "react-icons/io5";
+
+export default function AccountProfile({ showInputField }) {
+  // Assuming amount is the number of times the comment should be repeated
+ 
+
+
+
+  return (
+
+  );
+}
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -4,152 +4,195 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import AccountNavigation from "./AccountNavigation";
 import { Navigate, useParams } from "react-router-dom";
+import { MdApartment } from "react-icons/md";
+// import { FaHouseUser } from "react-icons/fa";
+// import { BsBuildingsFill } from "react-icons/bs";
+// import { GiFamilyHouse } from "react-icons/gi";
 
+export default function PlacesFormPage() {
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+  const [redirect, setRedirect] = useState(false);
+  const [price, setPrice] = useState(100);
 
-
-export default function PlacesFormPage(){
-    const {id} = useParams() ;
-    const [title, setTitle] = useState('');
-    const [address, setAddress] = useState('');
-    const [description, setDescription] = useState('');
-    const [addedPhotos, setAddedPhotos] = useState([]);
-    const [perks, setPerks] = useState([]);
-    const [extraInfo, setExtraInfo] = useState('');
-    const [checkIn, setCheckIn] = useState('');
-    const [checkOut, setCheckOut] = useState('');
-    const [maxGuests, setMaxGuests] = useState(1);
-    const [redirect, setRedirect] = useState(false);
-    const [price, setPrice] = useState(100);
-
-    useEffect(() =>{
-
-        if(!id)
-        {
-            return ;
-        }
-        axios.get('/places/' + id).then( response => {
-            const {data} = response ;
-            setTitle(data.title);
-            setAddress(data.address);
-            setDescription(data.description);
-            setAddedPhotos(data.photos)
-            setPerks(data.perks);
-            setExtraInfo(data.extraInfo);
-            setCheckIn(data.checkIn);
-            setCheckOut(data.checkOut);
-            setMaxGuests(data.maxGuests);
-            setPrice(data.price) ;
-           
-        });
-    },[id]) ;
-
-    function inputHead(text) {
-        return (
-            <h2 className="text-2xl mt-4">{text}</h2>
-        )
+  useEffect(() => {
+    if (!id) {
+      return;
     }
+    axios.get("/places/" + id).then((response) => {
+      const { data } = response;
+      setTitle(data.title);
+      setAddress(data.address);
+      setDescription(data.description);
+      setAddedPhotos(data.photos);
+      setPerks(data.perks);
+      setExtraInfo(data.extraInfo);
+      setCheckIn(data.checkIn);
+      setCheckOut(data.checkOut);
+      setMaxGuests(data.maxGuests);
+      setPrice(data.price);
+    });
+  }, [id]);
 
-    function inputDescription(text) {
-        return (
-            <p className="text-gray-500 text-sm">{text}</p>
-        )
+  function inputHead(text) {
+    return <h2 className="text-2xl mt-4">{text}</h2>;
+  }
+
+  function inputDescription(text) {
+    return <p className="text-gray-500 text-sm">{text}</p>;
+  }
+
+  function preInput(header, description) {
+    return (
+      <div>
+        {inputHead(header)}
+        {inputDescription(description)}
+      </div>
+    );
+  }
+
+  async function savePlace(ev) {
+    ev.preventDefault();
+    const placeData = {
+      title,
+      address,
+      addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    };
+
+    if (id) {
+      await axios.put("/places/:id", { id, ...placeData });
+      setRedirect(true);
+    } else {
+      await axios.post("/places/new", { placeData });
+      setRedirect(true);
     }
+  }
 
-    function preInput(header, description) {
-        return (
-            <div>
-                {inputHead(header)}
-                {inputDescription(description)}
-            </div>
-        )
-    }
+  if (redirect) {
+    return <Navigate to={"/account/places"} />;
+  }
 
-    
-   async function savePlace(ev)
-    {
-        ev.preventDefault();
-        const placeData = {  
-             title, address, addedPhotos ,description, perks, extraInfo, checkIn, checkOut, maxGuests, price
-        };
+  return (
+    <div>
+      <AccountNavigation />
 
-        if (id) {
-           
-            await axios.put('/places/:id',  {id, ...placeData});
-            setRedirect(true);
-            
-        } else {
-            
-            await axios.post('/places/new', { placeData});
-            setRedirect(true);
-        }
-        
-    }
+      <form onSubmit={savePlace}>
+        {preInput("Title", "Give your accomodation a suitable heading")}
+        <input
+          type="text"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+          placeholder="Title: Your accomodation's title"
+        />
 
-    if (redirect) {
-        return <Navigate to = {'/account/places'} />
-    }
+        {preInput("Address", "Give the physical address of your accomodation")}
+        <input
+          type="text"
+          value={address}
+          onChange={(ev) => setAddress(ev.target.value)}
+          placeholder="Title: Your accomodation's address"
+        />
 
-    return(
-        <div>
-            < AccountNavigation />
+        {preInput(
+          "Property type",
+          "What type of property best describes where you are listing your available rooms?"
+        )}
+        <div className=" flex   justify-center items-center gap-4 pt-2">
+          <div className=" flex flex-col items-center justify-center  max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100   dark:hover:bg-gray-200">
+            <MdApartment /> Apartment
+          </div>
+          <div className=" flex flex-col items-center justify-center  max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100   dark:hover:bg-gray-200">
+            <MdApartment /> Apartment
+          </div>
 
-        <form onSubmit={savePlace}>
-            {preInput("Title", "Give your accomodation a suitable heading")}
-            <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="Title: Your accomodation's title" />
+          <div className=" flex flex-col items-center justify-center  max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100   dark:hover:bg-gray-200">
+            <MdApartment /> Apartment
+          </div>
+          <div className=" flex flex-col items-center justify-center  max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100   dark:hover:bg-gray-200">
+            <MdApartment /> Apartment
+          </div>
+        </div>
 
-            {preInput('Address', 'Give the physical address of your accomodation')}
-            <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="Title: Your accomodation's address" />
+        {preInput("Photos", "Add captivating photos for your accomodation")}
+        <PhotoUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
 
-            {preInput('Photos', 'Add captivating photos for your accomodation')}
+        {preInput("Description", "Add Description for your accomodation")}
+        <textarea
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        ></textarea>
 
+        {preInput("Perks", "Select all the perks for your accomodation")}
+        <div className="mt-2 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+          <Perks selected={perks} onChange={setPerks} />
+        </div>
 
-            <PhotoUploader addedPhotos={addedPhotos} onChange = {setAddedPhotos} /> 
+        {preInput("Extra info", "House rules")}
+        <textarea
+          value={extraInfo}
+          onChange={(ev) => setExtraInfo(ev.target.value)}
+        ></textarea>
 
+        {preInput(
+          "Check in and out times and max guests",
+          "Add check in and checkout times"
+        )}
+        <div className="grid  grid-cols-3 gap-2 md:grid-cols-4">
+          <div>
+            <h3 className="mt-2 -mb-1">Check in time</h3>
+            <input
+              type="text"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+              placeholder="14:00"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Check out time</h3>
+            <input
+              type="text"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+              placeholder="11:00"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1">Max number of guests </h3>
+            <input
+              type="number"
+              value={maxGuests}
+              onChange={(ev) => setMaxGuests(ev.target.value)}
+              placeholder="5 Guests"
+            />
+          </div>
 
-            {preInput('Description', 'Add Description for your accomodation')}
-            <textarea value={description}
-                onChange={ev => setDescription(ev.target.value)}  ></textarea>
-
-            {preInput('Perks', 'Select all the perks for your accomodation')}
-            <div className="mt-2 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
-                <Perks selected={perks} onChange={setPerks} />
-            </div>
-
-            {preInput('Extra info', 'House rules')}
-            <textarea value={extraInfo}
-                onChange={ev => setExtraInfo(ev.target.value)}  ></textarea>
-
-            {preInput('Check in and out times and max guests', 'Add check in and checkout times')}
-            <div className="grid  grid-cols-3 gap-2 md:grid-cols-4">
-                <div>
-                    <h3 className="mt-2 -mb-1">Check in time</h3>
-                    <input type="text"
-                        value={checkIn}
-                        onChange={ev => setCheckIn(ev.target.value)}
-                        placeholder="14:00" />
-                </div>
-                <div>
-                    <h3 className="mt-2 -mb-1">Check out time</h3>
-                    <input type="text"
-                        value={checkOut}
-                        onChange={ev => setCheckOut(ev.target.value)}
-                        placeholder="11:00" />
-                </div>
-                <div>
-                    <h3 className="mt-2 -mb-1">Max number of guests </h3>
-                    <input type="number" value={maxGuests}
-                        onChange={ev => setMaxGuests(ev.target.value)} placeholder="5 Guests" />
-                </div>
-
-                <div>
-                    <h3 className="mt-2 -mb-1">Price per night </h3>
-                    <input type="number" value={price}
-                        onChange={ev => setPrice(ev.target.value)} placeholder="5 Guests" />
-                </div>
-
-            </div>
-            <button className="primary my-4">Save</button>
-        </form>
+          <div>
+            <h3 className="mt-2 -mb-1">Price per night </h3>
+            <input
+              type="number"
+              value={price}
+              onChange={(ev) => setPrice(ev.target.value)}
+              placeholder="5 Guests"
+            />
+          </div>
+        </div>
+        <button className="primary my-4">Save</button>
+      </form>
     </div>
-    )
+  );
 }
