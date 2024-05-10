298a08cda815647fe326dfe804490cfd7fdff35f
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-05-10 20:58:52 +0200

    Added icon maps

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -4,30 +4,41 @@ import { useParams } from "react-router-dom";
 import BookingWidget from "../BookingWidget";
 import PlaceGallery from "../PlaceGallery";
 import LocationAddress from "../LocationAddress";
-
+import { IoWifi } from "react-icons/io5";
 import {
   PiCertificate,
+  PiHouseLineThin,
   PiCouch,
   PiStudentBold,
   PiTelevisionSimpleThin,
   PiUsersThree,
+  PiBuildingApartment,
 } from "react-icons/pi";
-import { IoBedOutline, IoWifi } from "react-icons/io5";
-import { FaRegUser, FaStar, FaSwimmingPool } from "react-icons/fa";
-import { MdOutlineBedroomChild } from "react-icons/md";
-import { HiHome, HiHomeModern } from "react-icons/hi2";
-import { MdApartment } from "react-icons/md";
-import { CgGym } from "react-icons/cg";
-import { MdDeck } from "react-icons/md";
-import { GoPeople } from "react-icons/go";
-import { PiBedBold } from "react-icons/pi";
-import { MdOutlineYard } from "react-icons/md";
+// import { IoBedOutline, IoWifi } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
+// import { MdOutlineBedroomChild } from "react-icons/md";
+// import { HiHome, HiHomeModern } from "react-icons/hi2";
+// import { MdApartment } from "react-icons/md";
+// import { CgGym } from "react-icons/cg";
+// import { MdDeck } from "react-icons/md";
+// import { GoPeople } from "react-icons/go";
+// import { PiBedBold } from "react-icons/pi";
+// import { MdOutlineYard } from "react-icons/md";
+// import { TbFridge } from "react-icons/tb";
+// import { LuMicrowave } from "react-icons/lu";
+// import { GiToaster } from "react-icons/gi";
+// import { BsPersonLinesFill } from "react-icons/bs";
+import { IoBedOutline } from "react-icons/io5";
+
+import { LuMicrowave } from "react-icons/lu";
+import { TbFridge } from "react-icons/tb";
+import { GiToaster } from "react-icons/gi";
+
+import { FaRegUser } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { FaRegBuilding } from "react-icons/fa";
 
 import Comments from "../Comments";
-import { TbFridge } from "react-icons/tb";
-import { LuMicrowave } from "react-icons/lu";
-import { GiToaster } from "react-icons/gi";
-import { BsPersonLinesFill } from "react-icons/bs";
 
 export default function PlacePage() {
   const { id } = useParams();
@@ -41,6 +52,31 @@ export default function PlacePage() {
   }, [id]);
 
   if (!place) return "";
+
+  const placeOffersIconMap = {
+    "Wi Fi included": IoWifi,
+    Furnished: PiCouch,
+    "NSFAS Accredited": PiCertificate,
+    House: PiHouseLineThin,
+    Yard: FaRegBuilding,
+    Apartment: PiBuildingApartment,
+  };
+
+  const furnitureIncludedIconMap = {
+    Fridge: TbFridge,
+    Tv: PiTelevisionSimpleThin,
+    Microwave: LuMicrowave,
+    Bed: IoBedOutline,
+    Couch: PiCouch,
+    Toaster: GiToaster,
+  };
+  const preferredIconMap = {
+    Undergraduate: FaRegUser,
+    "Post Graduate": PiStudentBold,
+    "Young Professionals": PiUsersThree,
+    "Exchange Student": BsPersonLinesFill,
+    Other: FaRegUser,
+  };
 
   return (
     <div className=" mt-4 mx-8 px-12 pt-4 ">
@@ -70,50 +106,18 @@ export default function PlacePage() {
             <h2 className="fonr-semibold text-2xl">Description</h2>
             {place.description}
           </div>
-          {/* Check-in: {place.checkIn} <br />
-          Check-out: {place.checkOut} <br />
-          Max number of guests: {place.maxGuests} */}
+
           <h3 className="fonr-semibold text-2xl">What this place offers</h3>
           <div className="grid  grid-cols-3 gap-2 md:grid-cols-4 py-4">
-            <label className="flex items-baseline gap-2">
-              <HiHome /> House
-            </label>
-            {/* <label className="flex items-baseline gap-2">
-              <HiHomeModern /> Modern House
-            </label>
-            <label className="flex items-baseline gap-2">
-              <MdApartment /> Apartment
-            </label>
-            <label className="flex items-baseline gap-2">
-              <IoWifi /> Wi Fi included
-            </label> */}
-            <label className="flex items-baseline gap-2">
-              <PiCertificate /> NSFAS Accredited
-            </label>
-            {/* <label className="flex items-baseline gap-2">
-              <MdOutlineBedroomChild /> Bachelor
-            </label> */}
-            <label className="flex items-baseline gap-2">
-              <PiBedBold /> 1 Bedroom
-            </label>
-            {/* <label className="flex items-baseline gap-2">
-              <GoPeople /> Shared
-            </label> */}
-            <label className="flex items-baseline gap-2">
-              <PiCouch /> Furnished
-            </label>
-            <label className="flex items-baseline gap-2">
-              <MdOutlineYard /> Yard
-            </label>
-            {/* <label className="flex items-baseline gap-2">
-              <FaSwimmingPool /> Swimming Pool
-            </label>
-            <label className="flex items-baseline gap-2">
-              <CgGym /> Gym
-            </label>
-            <label className="flex items-baseline gap-2">
-              <MdDeck /> Deck
-            </label> */}
+            {Object.entries(placeOffersIconMap).map(
+              ([property, IconComponent]) =>
+                (place.placeOffers.includes(property) ||
+                  place.preferredTenants.includes(property)) && (
+                  <label className="flex items-baseline gap-2" key={property}>
+                    <IconComponent /> {property}
+                  </label>
+                )
+            )}
           </div>
           <h2 className="fonr-semibold text-2xl">Things to know</h2>
           {place.extraInfo}
@@ -121,24 +125,15 @@ export default function PlacePage() {
             Furniture & appliances included
           </h2>
           <div className="grid grid-cols-2 grid-flow-row gap-4 mb-6">
-            <div className="flex items-center gap-2">
-              <TbFridge /> Fridge
-            </div>
-            {/* <div className="flex items-center gap-2">
-              <PiTelevisionSimpleThin /> Tv
-            </div> */}
-            <div className="flex items-center gap-2">
-              <LuMicrowave /> Microwave
-            </div>
-            {/* <div className="flex items-center gap-2">
-              <IoBedOutline /> Bed
-            </div>
-            <div className="flex items-center gap-2">
-              <PiCouch /> Couch
-            </div>
-            <div className="flex items-center gap-2">
-              <GiToaster /> Toaster
-            </div> */}
+            {Object.entries(furnitureIncludedIconMap).map(
+              ([property, IconComponent]) =>
+                (place.placeOffers.includes(property) ||
+                  place.preferredTenants.includes(property)) && (
+                  <label className="flex items-baseline gap-2" key={property}>
+                    <IconComponent /> {property}
+                  </label>
+                )
+            )}
           </div>
           <button className=" bg-white border flex gap-2 items-center justify-center p-6 text-sm font-semibold">
             Show all
@@ -148,21 +143,15 @@ export default function PlacePage() {
           </h2>
 
           <div className="grid grid-cols-2 grid-flow-row gap-4 mb-6">
-            <div className="flex items-center gap-2">
-              <FaRegUser /> Undergraduate
-            </div>
-            <div className="flex items-center gap-2">
-              <PiStudentBold /> Post Graduate
-            </div>
-            {/* <div className="flex items-center gap-2">
-              <PiUsersThree /> Young Professionals
-            </div>
-            <div className="flex items-center gap-2">
-              <BsPersonLinesFill /> Exchange Student
-            </div>
-            <div className="flex items-center gap-2">
-              <FaRegUser /> Other
-            </div> */}
+            {Object.entries(preferredIconMap).map(
+              ([property, IconComponent]) =>
+                (place.placeOffers.includes(property) ||
+                  place.preferredTenants.includes(property)) && (
+                  <label className="flex items-baseline gap-2" key={property}>
+                    <IconComponent /> {property}
+                  </label>
+                )
+            )}
           </div>
         </div>
         <div className="  sticky">
